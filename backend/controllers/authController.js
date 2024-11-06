96c13e98a513e6d0f6707fc34f6de05036208fb8
Lanseeg <38041866+Lanseeg@users.noreply.github.com>
2024-11-06 22:39:09 +0100

    feat. controllers/authController uses bcrypt & jsonwebtoken for improved security. Middleware  auth.js manages .env JWT (json web token). RESTful routes managed by app.js & authRoutes and bookRoutes (POST & GET)

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -17,15 +17,26 @@ exports.signup = async (req, res) => {
 // Controller for user login
 exports.login = async (req, res) => {
   try {
+    // Find user email
     const user = await User.findOne({ email: req.body.email });
-    if (!user) return res.status(401).json({ message: 'Invalid email or password' });
+    if (!user) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
 
+    // Compare provided with hashed database password
     const validPassword = await bcrypt.compare(req.body.password, user.password);
-    if (!validPassword) return res.status(401).json({ message: 'Invalid email or password' });
+    if (!validPassword) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
 
+    // Generate a token with the user's ID
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
+
+    // Respond with user ID and token
     res.status(200).json({ userId: user._id, token });
   } catch (error) {
+    // Log the error and return a 500 status
+    console.error('Login error:', error);
     res.status(500).json({ message: 'Failed to login', error });
   }
 };
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,9 +3,11 @@ const jwt = require('jsonwebtoken');
 // Middleware to verify JWT token
 module.exports = (req, res, next) => {
   try {
-    const token = req.headers.authorization.split(' ')[1]; // Get token from header
+    const token = req.headers.authorization.split(' ')[1];
+    
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
-    req.userId = decodedToken.userId; // Set user ID from token
+
+    req.userId = decodedToken.userId;
     next();
   } catch (error) {
     res.status(403).json({ message: 'Unauthorized request' });
