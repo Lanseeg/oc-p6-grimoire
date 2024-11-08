c46079e058bcbeade7d3a3f4e844c6ffe7765cef
Lanseeg <38041866+Lanseeg@users.noreply.github.com>
2024-11-09 00:06:07 +0100

    feat. Added sharp for image compression. Got a   errno: -4048  code: EPERM,syscall:unlink on windows OS, to be fixed later

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,4 +1,8 @@
+// middleware/multer-config.js
 const multer = require('multer');
+const path = require('path');
+const fs = require('fs');
+const sharp = require('sharp');
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
@@ -11,10 +15,55 @@ const storage = multer.diskStorage({
     callback(null, 'images');
   },
   filename: (req, file, callback) => {
-    const name = file.originalname.split(' ').join('_');
+    const name = file.originalname.split(' ').join('_').split('.')[0];
     const extension = MIME_TYPES[file.mimetype];
     callback(null, name + Date.now() + '.' + extension);
   }
 });
 
-module.exports = multer({storage: storage}).single('image');
+const upload = multer({ storage: storage }).single('image');
+
+// Middleware to handle image compression with Sharp
+const uploadAndCompressImage = (req, res, next) => {
+    upload(req, res, async (err) => {
+      if (err) {
+        return res.status(500).json({ error: 'Failed to upload image' });
+      }
+      if (!req.file) {
+        return next();
+      }
+  
+      const { path: tempPath, filename } = req.file;
+      const compressedImagePath = `images/compressed-${filename}`;
+  
+      try {
+        // Sharp for compressing images
+        await sharp(tempPath)
+          .resize(800, 600)
+          .jpeg({ quality: 80 })
+          .toFile(compressedImagePath);
+  
+        console.log('Image compressed successfully');
+  
+        // Deleted remaining file
+        if (fs.existsSync(tempPath)) {
+            fs.unlink(tempPath, (err) => {
+              if (err) {
+                console.error('Failed to delete original image:', err);
+              } else {
+                console.log('Original image deleted');
+              }
+            });
+          }
+  
+        req.file.path = compressedImagePath;
+        req.file.filename = `compressed-${filename}`;
+        next();
+      } catch (error) {
+        console.error('Error during image processing with Sharp:', error);
+        return res.status(500).json({ error: 'Failed to process image' });
+      }
+    });
+  };
+
+module.exports = uploadAndCompressImage;
--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -9,7 +9,7 @@ router.get('/', bookController.getAllBooks);
 router.get('/:id', bookController.getBookById);
 router.get('/bestrating', bookController.getBestRatedBooks);
 router.post('/', auth, multer, bookController.createBook);
-router.put('/:id', auth, bookController.updateBook);
+router.put('/:id', auth, multer, bookController.updateBook);
 router.delete('/:id', auth, bookController.deleteBook);
 router.post('/:id/rating', auth, bookController.addRating);
 
